8d5ef64b37bb129fe77e1db1eecc77c82f7c5226
KK <2693289352@qq.com>
2026-08-11 21:44:54 +0800

    chore: bump version to 1.3.2 for release

--- IronNestFCS/FcsHostMod.cs
+++ IronNestFCS/FcsHostMod.cs
@@ -5,7 +5,7 @@ using MelonLoader.Utils;
 using UnityEngine;
 using UnityEngine.InputSystem;
 
-[assembly: MelonInfo(typeof(FcsHostMod), "IronNestFCS", "1.3.1", "KK")]
+[assembly: MelonInfo(typeof(FcsHostMod), "IronNestFCS", "1.3.2", "KK")]
 [assembly: MelonGame("Iron Nest", "Iron Nest Heavy Turret Simulator")]
 
 namespace IronNestFCS;
